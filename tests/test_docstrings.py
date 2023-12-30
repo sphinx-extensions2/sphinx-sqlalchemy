f81676cac5324570cc9a25a29cb40d0b3e404e2b
Alexis Tourneux <tourneuxalexis@gmail.com>
2023-12-30 16:44:30 +0100

    [FIX] typo in test docstring #14

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -6,7 +6,7 @@ from sphinx_pytest.plugin import CreateDoctree
 
 
 def test_multiline_docstring_d212(sphinx_doctree_no_tr: CreateDoctree, snapshot):
-    """Basic test for molde with multiline string with D212 format"""
+    """Basic test for models with multiline string with D212 format"""
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), "modules"))
     sphinx_doctree_no_tr.set_conf({"extensions": ["sphinx_sqlalchemy"]})
     result = sphinx_doctree_no_tr(".. sqla-model:: module2.TestUserMultilineDocstringD212")
@@ -14,7 +14,7 @@ def test_multiline_docstring_d212(sphinx_doctree_no_tr: CreateDoctree, snapshot)
     assert "\n".join([li.rstrip() for li in result.pformat().splitlines()]) == snapshot
 
 def test_multiline_docstring_d213(sphinx_doctree_no_tr: CreateDoctree, snapshot):
-    """Basic test for molde with multiline string with D213 format"""
+    """Basic test for models with multiline string with D213 format"""
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), "modules"))
     sphinx_doctree_no_tr.set_conf({"extensions": ["sphinx_sqlalchemy"]})
     result = sphinx_doctree_no_tr(".. sqla-model:: module2.TestUserMultilineDocstringD213")
